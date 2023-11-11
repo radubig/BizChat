e0b0e2706fbdea5346a1efc73a0c4e620649fee9
Coltos Radu <radubig1987@gmail.com>
2023-11-11 19:17:15 +0200

    Setup project

---
+++ BizChat/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BizChat.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
